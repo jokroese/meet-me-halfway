4f75ed84261d5b3ff92c8b59a83a4de6afa2af3b
SebStrug <strugsebastian@gmail.com>
2017-11-11 15:41:49 +0000

    Added suggester

--- scrBrowseRoutes.py
+++ scrBrowseRoutes.py
@@ -6,9 +6,15 @@ Created on Sat Nov 11 12:08:09 2017
 """
 
 import requests
+import json
 
 api_key = 'ha177649362715475514428886582394'
 
+def convertString(zippedList,name):
+    """Converts a country name to the corresponding country code"""
+    #e.g. Russia --> RU
+    codeIndex = [i[1] for i in zippedList].index(name)
+    return zippedList[codeIndex][0]
 
 #basicurl = 'http://partners.api.skyscanner.net/apiservices/browsequotes/v1.0/'
 refurl = "http://partners.api.skyscanner.net/apiservices/"
@@ -16,17 +22,25 @@ locales = requests.get(refurl+"reference/v1.0/locales?apiKey="+api_key)
 markets = requests.get(refurl+"reference/v1.0/countries/en-GB?apiKey="+api_key)
 currencies = requests.get(refurl+"reference/v1.0/currencies?apiKey="+api_key)
 
+def suggester(query):
+    """Returns suggestions if the input is incorrect"""
+    #query = 'fran'
+    autoSuggest = requests.get(refurl+"autosuggest/v1.0/RU/USD/en-GB?query="+query+"&apiKey="+api_key)
+    autoSuggJSON = json.loads(autoSuggest.text)
+    print('Did you mean...')
+    for i in autoSuggJSON['Places']:
+        print(i['PlaceName'])
 
-autoSuggest = requests.get(refurl+"autosuggest/v1.0/RU/USD/en-GB?query={fran}&apiKey="+api_key)
 country = 'RU'
 locale = 'en-GB'
 currency = 'USD'
 originPlace = 'RU'
-destinationPlace = 'PT'
+destinationPlace = 'ZIEL'
 outboundPartialDate = '2017-11'
 inboundPartialDate = '2017-11-10'
 
 browseRoutes = requests.get(refurl+"/browseroutes/v1.0/"+country+"/"+currency+"/"+locale+"/"+originPlace+"/"+destinationPlace+"/"+outboundPartialDate+"/?apiKey="+api_key)
+
 
 #browseQuotes = requests.get(refurl+"browsequotes/v1.0/\
 #                           {"+country+"}/{"+currency+"}/{"+locale+"}\
